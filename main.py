250935379e3b6d8687c9580b63ba0f7815bfcf95
thmastin <thmastin@gmail.com>
2024-08-22 23:09:43 -0400

    Add player triangle

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 import pygame
 
 from constants import *
+from player import *
+from circleshape import *
 
 def main():
     pygame.init()
@@ -11,15 +13,18 @@ def main():
     dt = 0
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
         dt = fps.tick(60) / 1000
-        print(dt)
+        
+
         
         #print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
