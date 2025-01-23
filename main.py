be0d78afd8cd5010e7223769bf55e1d1b97b117c
thmastin <thmastin@gmail.com>
2025-01-22 20:50:09 -0500

    Add player rotation

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from player import *
 pygame.init()
 
 clock = pygame.time.Clock()
-dt = 0
 
 def main():
     print("Starting asteroids!")
@@ -18,14 +17,16 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
+    dt = 0
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
+        
         screen.fill((0, 0, 0))
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         clock.tick(60)
