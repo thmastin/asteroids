087c053dd2e751829f19c971ac6a525da864bddc
thmastin <thmastin@gmail.com>
2025-01-23 07:46:41 -0500

    Move player instances to groups for updating and drawing

--- main.py
+++ main.py
@@ -14,7 +14,13 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
+    
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updateable, drawable)
+    
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
@@ -26,8 +32,11 @@ def main():
         
         
         screen.fill((0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
+        for thing in updateable:
+            thing.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
+        
         pygame.display.flip()
         clock.tick(60)
         dt = clock.tick(60) / 1000
