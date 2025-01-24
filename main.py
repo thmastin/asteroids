864e38a11c0a6774ba9cafad4d082e4a7a3ba110
thmastin <thmastin@gmail.com>
2025-01-24 01:09:20 -0500

    Add larger asteroids spliting when hit

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def main():
         for asteroid in asteroids:
             for shot in shots:
                 if asteroid.collision(shot):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
 
         for thing in drawable:
